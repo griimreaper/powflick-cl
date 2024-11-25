219b1242b1ca7191652d76377ce57356bf03d159
Clay Martinez <clay.ms@hotmail.com>
2024-11-25 13:07:23 -0500

    style: change images

--- src/pages-sections/fashion-2/section-1/section-1.tsx
+++ src/pages-sections/fashion-2/section-1/section-1.tsx
@@ -19,7 +19,7 @@ export default async function Section1({ data }: { data: DataStructure["navbar"]
               key={ind}
               buttonColor="dark"
               title={item.title}
-              imgUrl={item.imgUrl}
+              imgUrl="assets/images/products/sports2.png"
               buttonLik={item.buttonLik}
               buttonText={item.buttonText}
               description={item.description}
