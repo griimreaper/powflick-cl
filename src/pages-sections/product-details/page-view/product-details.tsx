2cb7db8892daddce465c9be743c66b19c29ee5ee
Clay Martinez <clay.ms@hotmail.com>
2025-01-29 13:42:05 -0500

    style: bg dashboard

--- src/pages-sections/product-details/page-view/product-details.tsx
+++ src/pages-sections/product-details/page-view/product-details.tsx
@@ -15,7 +15,7 @@ export default function ProductDetailsPageView({ detail }: { detail: detailProps
   const { product, sugestedProducts, frequentlyBought, reviews } = detail;
 
   return (
-    <Container className="pt-4 mb-2" sx={{ overflow: 'hidden', background: "white"} }>
+    <div className="p-4 mb-2" style={{ overflow: 'hidden', background: "white"} }>
       {/* PRODUCT DETAILS INFO AREA */}
       <ProductIntro product={detail} />
 
@@ -29,6 +29,6 @@ export default function ProductDetailsPageView({ detail }: { detail: detailProps
       {/* <AvailableShops /> */}
       {/* RELATED PRODUCTS AREA */}
       <RelatedProducts products={sugestedProducts} />
-    </Container>
+    </div>
   );
 }
