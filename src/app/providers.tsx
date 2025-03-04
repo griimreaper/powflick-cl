808fec58cf8aaa8a6aa63cf3c7d1868dc334b70f
Griimreaper <leonelbehnke@gmail.com>
2025-03-04 13:09:17 -0300

    fix: deploy

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,10 @@
 'use client';
 import ProgressBar from "components/progress";
-import RTL from "components/rtl";
+// import RTL from "components/rtl";
 import CartProvider from "contexts/CartContext";
 import ClientProviders from "contexts/ClientProviders";
 import { NavbarProvider } from "contexts/NavBarContext";
-import SettingsProvider from "contexts/SettingContext";
+// import SettingsProvider from "contexts/SettingContext";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
 import ThemeProvider from "theme/theme-provider";
--- src/components/rtl/index.ts
+++ src/components/rtl/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./RTL";
+// export { default } from "./RTL";
