76e8bdd461ba0b7538af5e4b958be72d3aec7dbc
Griimreaper <leonelbehnke@gmail.com>
2025-06-12 11:23:09 -0300

    fix: footer spacing

--- src/components/footer/footer-1.tsx
+++ src/components/footer/footer-1.tsx
@@ -166,26 +166,26 @@ export default function Footer1({ data }: { data: DataStructure["navbar"] }) {
                 {/* CONTACT INFORMATION */}
                 <Heading style={{ color: "#CA0B0B", marginBottom: "24px" }}>Contact Us</Heading>
 
-                <Paragraph py={0.6} color="grey.500">
+                <Paragraph py={1.4} color="grey.500">
                   <LocationOnIcon
                     sx={{ fontSize: 18, verticalAlign: "middle", mr: 1 }}
                   />
                   RM C, 6/F, WORLD TRUST TOWER, 50 STANLEY STREET, CENTRAL HK
                 </Paragraph>
 
-                <Paragraph py={0.6} color="grey.500">
+                <Paragraph py={1.4} color="grey.500">
                   Registered Office (Legal Entity):
                   CL 28 #113 57, Cali – Valle, Colombia
                   NIT: 1231234
 
                 </Paragraph>
 
-                <Paragraph py={0.6} color="grey.500">
+                <Paragraph py={1.4} color="grey.500">
                   <EmailIcon sx={{ fontSize: 18, verticalAlign: "middle", mr: 1 }} />
                   Email: support@powflick.com
                 </Paragraph>
 
-                <Paragraph py={0.6} mb={2} color="grey.500">
+                <Paragraph py={1.4} mb={2} color="grey.500">
                   <PhoneIcon sx={{ fontSize: 18, verticalAlign: "middle", mr: 1 }} />
                   Phone: +86 13590635129
                 </Paragraph>
